922cc79910273211c522f5559b8701ed1bd78957
joecoxstrong <joecoxstrong@gmail.com>
2022-05-04 11:48:33 -0400

    Created customer views.py + Added functions for CRUD functionality

--- backend/customer/views.py
+++ backend/customer/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
+from addon import serializers
+from .models import Customer
+from .serializers import CustomerSerializer
+
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def customer_list(request):
+    if request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    elif request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def customer_details(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+    elif request.method == 'Put':
+        serializer = CustomerSerializer(Customer, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
